34e604ea08cd3caf91ab86e2d025e6fe6257055a
Alan <aljiar23@gmail.com>
2017-06-01 09:14:59 -0400

    Proceso Iterativo

--- tiendaTabs/src/pages/home/home.ts
+++ tiendaTabs/src/pages/home/home.ts
@@ -8,9 +8,10 @@ import {ListaCategoriaPage} from '../lista-categoria/lista-categoria';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
+  categorias=[{name: "Lacteos",image: "assets/img/lacteos.jpg"},
+              {name: "Carnes",image: "assets/img/carnes.jpg"},
+              {name: "Frutas",image: "assets/img/frutasyverduras.jpg"}];
   constructor(public navCtrl: NavController) {
-
   }
   
   pushPage()
